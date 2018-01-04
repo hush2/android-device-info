96673eab89c4a9affe5c14b5a9e0f91d6690e87b
Jeff <hush2dev@gmail.com>
2018-01-04 14:40:28 +0800

    Use container view for each screen

---
+++ src/components/RowContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, ScrollView, StyleSheet } from 'react-native'
+
+export default class RowContainer extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView>{this.props.children}</ScrollView>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+  },
+})
