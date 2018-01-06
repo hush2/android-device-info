3c57c436eb39f9d4de362505eb11ff1be4d876fa
Jeff <hush2dev@gmail.com>
2018-01-06 16:15:11 +0800

    Refactor sim screen

--- src/screens/SIM.js
+++ src/screens/SIM.js
@@ -11,21 +11,21 @@ export default class SIM extends React.Component {
     tabBarIcon: () => <Icon name="sim" size={25} color="white" />,
   }
 
-  state = { loading: true }
+  state = { ready: false }
 
   async componentDidMount() {
     const device = NativeModules.RNAndroidDeviceInfo
     try {
       this.sim = await device.getSimInfo()
       this.device = await device.getDeviceInfo()
+      this.setState({ ready: true })
     } catch (err) {
       console.log(err)
     }
-    this.setState({ loading: false })
   }
 
   render() {
-    if (this.state.loading) {
+    if (!this.state.ready) {
       return <Loading />
     }
 
@@ -35,8 +35,8 @@ export default class SIM extends React.Component {
         <RowItem title="IMSI" value={this.sim.imsi} />
         <RowItem title="Serial" value={this.sim.serial} />
         <RowItem title="Country" value={this.sim.country} />
-        <RowItem title="SIM Network Locked" value={this.sim.isSimNetworkLocked} />
-        <RowItem title="Multi SIM" value={this.sim.isMultiSim} />
+        <RowItem title="SIM Network Locked?" value={this.sim.isSimNetworkLocked} />
+        <RowItem title="Multi SIM?" value={this.sim.isMultiSim} />
         <RowItem title="# of Active SIM" value={this.sim.numberOfActiveSim} />
       </RowContainer>
     )
