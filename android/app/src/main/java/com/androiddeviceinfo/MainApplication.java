634f5032e30e4003deb6881d8c8b2c58d5a55342
Jeff <hush2dev@gmail.com>
2018-01-06 11:20:55 +0800

    Update module name

--- android/app/src/main/java/com/androiddeviceinfo/MainApplication.java
+++ android/app/src/main/java/com/androiddeviceinfo/MainApplication.java
@@ -3,7 +3,7 @@ package com.androiddeviceinfo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.github.hush2.RNAndroidDeviceInfo.RNEasyDeviceInfoPackage;
+import com.github.hush2.RNAndroidDeviceInfo.RNAndroidDeviceInfoPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,7 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNEasyDeviceInfoPackage()
+            new RNAndroidDeviceInfoPackage()
       );
     }
 
